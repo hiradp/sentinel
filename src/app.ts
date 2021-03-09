362c36d8f678ac81094b2d8ce88f3781e0fbbb02
Hirad Pourtahmasbi <git@hirad.codes>
2021-03-08 22:50:32 -0800

    feat: sentiment analyzer

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { App } from '@slack/bolt';
 import * as dotenv from 'dotenv';
+import { analyzeSentiment } from './sentiment';
 
 // Load environment variables
 dotenv.config();
@@ -10,11 +11,13 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-// Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-  console.log(message);
+// Listens to all messages
+app.message(/(.*?)/, async ({ message, say }) => {
+  const body = message as any;
+  console.log(`text: ${body.text}`);
+  const sentiment = await analyzeSentiment(body.text);
   // say() sends a message to the channel where the event was triggered
-  await say(`Hey there!`);
+  await say(`Hey there! - ${sentiment}`);
 });
 
 (async () => {
---
+++ src/sentiment.ts
@@ -0,0 +1,31 @@
+// Imports the Google Cloud client library
+import * as language from '@google-cloud/language';
+
+
+export async function analyzeSentiment(text: string): Promise<number> {
+  // Creates a client
+  const client = new language.LanguageServiceClient();
+
+  // Prepares a document, representing the provided text
+  const document: any = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+  
+  // Detects the sentiment of the document
+  const [result] = await client.analyzeSentiment({
+    document,
+  });
+
+  const sentiment = result.documentSentiment;
+  if (sentiment) {
+    console.log('Document sentiment:');
+    console.log(`  Score: ${sentiment.score}`);
+    console.log(`  Magnitude: ${sentiment.magnitude}`);
+  } else {
+    throw new Error('unable to analyze sentiment');
+  }
+
+
+  return sentiment.score as number;
+}
